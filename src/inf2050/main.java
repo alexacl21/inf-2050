4ae1059f91d2272ad55ab92d686d93a15f6d0527
ARCOLE Alexandre-Thibault <arcole.alexandre-thibault@courrier.uqam.ca>
2019-09-23 11:16:46 -0400

    Liaison de git et Eclipse

---
+++ src/inf2050/main.java
@@ -0,0 +1,14 @@
+package inf2050;
+import java.util.stream.IntStream;
+
+public class main {
+
+		public static void main(String[] args) {
+			
+			IntStream.rangeClosed(1, 100)
+		    .mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i))
+		    .forEach(System.out::println);
+
+		}
+
+}

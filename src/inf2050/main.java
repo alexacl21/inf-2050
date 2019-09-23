3a2cb10fcf52de9c4d13b164692339a36502342f
ARCOLE Alexandre-Thibault <arcole.alexandre-thibault@courrier.uqam.ca>
2019-09-23 11:18:44 -0400

    Changement du range de 1 a 200

--- src/inf2050/main.java
+++ src/inf2050/main.java
@@ -5,7 +5,7 @@ public class main {
 
 		public static void main(String[] args) {
 			
-			IntStream.rangeClosed(1, 100)
+			IntStream.rangeClosed(1, 200)
 		    .mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i))
 		    .forEach(System.out::println);
 
